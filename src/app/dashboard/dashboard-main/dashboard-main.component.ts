9d4f8adafbae9bb8c195ed1e1453f6a924ca81e8
devilsnest-sys <sharmapranjal588@gmail.com>
2024-12-23 13:54:57 +0530

    changes

--- src/app/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/dashboard/dashboard-main/dashboard-main.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-main.component.css'
 })
 export class DashboardMainComponent {
-
+  steps = Array(5).fill(0); // Generates an array with 15 steps
+  currentStep = 3;
 }
--- src/app/dashboard/masterdocument/masterdocument.component.ts
+++ src/app/dashboard/masterdocument/masterdocument.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-masterdocument',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './masterdocument.component.css'
 })
 export class MasterdocumentComponent {
+  stageForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.stageForm = this.fb.group({
+      stageName: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.stageForm.valid) {
+      console.log('Form Submitted', this.stageForm.value);
+    }
+  }
+
+  onCancel(): void {
+    this.stageForm.reset();
+  }
 }
